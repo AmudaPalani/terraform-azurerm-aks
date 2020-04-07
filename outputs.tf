36998fb5df7865f6e63e98b130317770fb7f2faa
Amudha Palani <53053396+AmudaPalani@users.noreply.github.com>
2020-04-07 15:03:17 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,36 +1,11 @@
-output "client_key" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].client_key
-}
-
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].client_certificate
+  value = azurerm_kubernetes_cluster.dce_cluster.kube_config.0.client_certificate
 }
 
-output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate
+output "cluster_name" {
+  value = azurerm_kubernetes_cluster.dce_cluster.name
 }
 
-output "host" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].host
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.dce_cluster.kube_config_raw
 }
-
-output "username" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].username
-}
-
-output "password" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].password
-}
-
-output "node_resource_group" {
-  value = azurerm_kubernetes_cluster.main.node_resource_group
-}
-
-output "location" {
-  value = azurerm_kubernetes_cluster.main.location
-}
-
-output "aks_id" {
-  value = azurerm_kubernetes_cluster.main.id
-}
-
