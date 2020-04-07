dda78edaf4dda277bd27f754bd9bddd692d3d695
Amudha Palani <53053396+AmudaPalani@users.noreply.github.com>
2020-04-07 15:24:00 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,26 +12,26 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "{prefix}-terraform-vnet-rg"
+  name     = "${prefix}-terraform-vnet-rg"
   location = var.location
 }
 
 resource "azurerm_virtual_network" "network" {
-  name                = "{prefix}-wus-aks-vnet"
+  name                = "${prefix}-wus-aks-vnet"
   location            = "${azurerm_resource_group.rg.location}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   address_space       = ["10.1.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                      = "{prefix}-aks-subnet"
+  name                      = "${prefix}-aks-subnet"
   resource_group_name       = "${azurerm_resource_group.rg.name}"
   address_prefix            = "10.1.0.0/24"
   virtual_network_name      = "${azurerm_virtual_network.network.name}"
 }
 
 resource "azurerm_kubernetes_cluster" "dce_cluster" {
-  name       = "{prefix}-aks"
+  name       = "${prefix}-aks"
   location   = "${azurerm_resource_group.rg.location}"
   dns_prefix = "aks"
 
